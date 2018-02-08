96490261fc59fc75c7232c3a1b49e4d27e1b9cb4
quhuainan <1026323214@qq.com>
2018-02-08 10:57:04 +0800

    完成redux基本使用

--- first_react_app/src/chapter-01/ClickCounter.js
+++ first_react_app/src/chapter-01/ClickCounter.js
@@ -6,16 +6,27 @@ export default class ClickCounter extends Component {
         super(props)
         // 将函数方法与this绑定，防止在函数方法中调用的this 不是 该实例对象
         this.onClickButton = this.onClickButton.bind(this)
-        this.state = { count: 0 }
+        this.state = { count: 0, value: '', isZ: false }
     }
 
     onClickButton() {
-        this.setState({ count: ++this.state.count })
+        let float = new RegExp("^-?([1-9]\\d*\\.\\d*|0\\.\\d*[1-9]\\d*|0?\\.0+|0)$")
+        let int = new RegExp("^-?[1-9]\\d*$")
+        
+        let a=float.test(this.state.value)||int.test(this.state.value)
+        
+        this.setState({
+            count: ++this.state.count,
+            isZ: a
+        })
     }
     render() {
         return <div style={{ padding: 46 }}>
             <button onClick={this.onClickButton}>点击我</button>
+            <input value={this.state.value} onChange={(event) => { this.setState({ value: event.target.value }) }} />>
             <div style={{ paddingTop: 16 }}>点击次数：<span id="clickCount">{this.state.count}</span></div>
+            <div style={{ paddingTop: 16 }}>是否是整数：<span id="clickCount">{this.state.isZ ? "是整数" : "不是整数"}</span></div>
+
         </div>
     }
 }
--- first_react_app/src/index.js
+++ first_react_app/src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import ControlPanel from './chapter-03/src/views/ControlPanel'
-ReactDOM.render(<ControlPanel />, document.getElementById('root'));
+import ClickCounter from './chapter-01/ClickCounter'
+ReactDOM.render(<ClickCounter />, document.getElementById('root'));
 registerServiceWorker();
